ad2687883f65ab49e4bc129e465c1e1526b1aab9
Bomin Kwon <kbomin328@gmail.com>
2022-12-21 01:04:24 +0900

    login logout OK

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -4,7 +4,7 @@ let auth = (req, res, next) => {
     //인증 처리를 하는곳 
     //클라이언트 쿠키에서 토큰을 가져온다.
 
-    let token = req.cookies.x_auth;
+    let token = req.cookies.w_auth;
     // 토큰을 복호화 한후  유저를 찾는다.
     User.findByToken(token, (err, user) => {
         if (err) throw err;
@@ -17,7 +17,7 @@ let auth = (req, res, next) => {
         req.user = user;
         next();
     });
-}
+};
 
 
 module.exports = { auth };
